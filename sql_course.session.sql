a219d0305940efc983808c4190261448f9a36090
Omar Muniz <omiethehomie.om@gmail.com>
2026-01-05 10:08:00 -0600

    push example

---
+++ sql_course.session.sql
@@ -0,0 +1,30 @@
+
+SELECT 
+    q1_job_postings.job_title_short,
+    q1_job_postings.job_location,
+    q1_job_postings.salary_year_avg,
+    q1_job_postings.job_via,
+    q1_job_postings.job_posted_date :: DATE  
+ 
+
+FROM (
+    SELECT 
+        *
+    FROM 
+        january_jobs
+    UNION ALL
+    SELECT 
+        *
+    FROM 
+        february_jobs
+    UNION ALL
+    SELECT 
+        *
+    FROM 
+        march_jobs
+) AS q1_job_postings
+WHERE 
+        q1_job_postings.salary_year_avg > 70000 AND 
+        job_title_short = 'Data Analyst'
+ORDER BY 
+    salary_year_avg DESC 
